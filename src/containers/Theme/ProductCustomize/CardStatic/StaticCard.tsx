4c1ef654d2f4f5d787f0f045424c3ee86c15154e
gost1930 <hadbimohamed66@gmail.com>
2025-01-23 23:51:48 +0100

    fix StaticCard.tsx and add card

--- src/containers/Theme/ProductCustomize/CardStatic/StaticCard.tsx
+++ src/containers/Theme/ProductCustomize/CardStatic/StaticCard.tsx
@@ -13,9 +13,9 @@ const StaticCard = () => {
     },
     {
       name: "إفكت هوفر في الصورة",
-      parentClassName: "overflow-hidden hover:shadow-xl",
+      parentClassName: "overflow-hidden hover:shadow-xl group",
       parentImageClasseName: "overflow-hidden",
-      imageClassName: "hover:scale-110 transform",
+      imageClassName: "group-hover:scale-110 transform",
       discountClassName: "",
       btnClassName: "bg-black text-white hover:-translate-y-2",
     },
@@ -30,24 +30,24 @@ const StaticCard = () => {
     },
     {
       name: "إفكت هوفر الصورة ",
-      parentClassName: "",
+      parentClassName: "group hover:shadow-2xl",
       parentImageClasseName: "",
-      imageClassName: "hover:scale-110 transform",
+      imageClassName: "group-hover:scale-105 transform",
       discountClassName: "",
-      btnClassName: "",
+      btnClassName: "group-hover:bg-black group-hover:text-white",
     },
   ];
   return (
     <section>
       <Button text="حفظ" className="mt-10 w-32 h-fit bg-primary" />
       <div className="w-full h-fit p-3 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5">
-        {cardClasses.map((c, index) => (
+        {cardClasses.map((card, index) => (
           <div>
             <div>
-              <input type="radio" name="card" id={c.name} className="ml-2" />
-              <label htmlFor={c.name}>{c.name}</label>
+              <input type="radio" name="card" id={card.name} className="ml-2" />
+              <label htmlFor={card.name}>{card.name}</label>
             </div>
-            <Card key={index} {...c} />
+            <Card key={index} {...card} />
           </div>
         ))}
       </div>
